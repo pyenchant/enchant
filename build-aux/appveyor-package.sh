efa29858717340d59a7051f0d50bd96811adf9d6
Dimitri Merejkowsky <d.merej@gmail.com>
2020-03-01 14:05:53 +0100

    fix ident

--- build-aux/appveyor-package.sh
+++ build-aux/appveyor-package.sh
@@ -32,29 +32,28 @@ install() {
 }
 
 copy_dlls() {
-    dest="$PROJECT_DIR/dist/$MINGW_ARCH/bin/"
-    mkdir -p $dest
-    dlls=(
-      libgcc_s_seh-1.dll
-      libglib-2.0-0.dll
-      libgmodule-2.0-0.dll
-      libgobject-2.0-0.dll
-      libhunspell-1.6-0.dll
-      libiconv-2.dll
-      libintl-8.dll
-      libp11-kit-0.dll
-      libpcre-1.dll
-      libpcre16-0.dll
-      libpcre32-0.dll
-      libpcrecpp-0.dll
-      libpcreposix-0.dll
-      libstdc++-6.dll
-      libwinpthread-1.dll
-    )
-
-    for dll in "${dlls[@]}" ; do
-      cp -v /$MINGW_ARCH/bin/$dll $dest
-    done
+  dest="$PROJECT_DIR/dist/$MINGW_ARCH/bin/"
+  mkdir -p $dest
+  dlls=(
+    libgcc_s_seh-1.dll
+    libglib-2.0-0.dll
+    libgmodule-2.0-0.dll
+    libgobject-2.0-0.dll
+    libhunspell-1.6-0.dll
+    libiconv-2.dll
+    libintl-8.dll
+    libp11-kit-0.dll
+    libpcre-1.dll
+    libpcre16-0.dll
+    libpcre32-0.dll
+    libpcrecpp-0.dll
+    libpcreposix-0.dll
+    libstdc++-6.dll
+    libwinpthread-1.dll
+  )
+  for dll in "${dlls[@]}" ; do
+    cp -v /$MINGW_ARCH/bin/$dll $dest
+  done
 }
 
 copy_licenses() {
